c9f6b3663391a767a0659a02c4c4fb9f2c9d8797
jessiyang1996 <jessiyang1996@gmail.com>
2024-03-12 22:27:33 -0500

    added main header image

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -5,7 +5,15 @@ import './LandingPage.css';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
-  return <div className="container"></div>;
+  return (
+    <div className="landingPageContainer">
+      <div className="container">
+        {/* <img className="headerImage" src="/images/IMG_0055.jpg"></img> */}
+        <img className="headerImage" src="/images/tando-jeongok.jpeg"></img>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default LandingPage;
