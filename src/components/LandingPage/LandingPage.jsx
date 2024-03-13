34b740e08172665681968b1a4590001e72decbba
jessiyang1996 <jessiyang1996@gmail.com>
2024-03-12 22:57:48 -0500

    added lines

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -11,7 +11,15 @@ function LandingPage() {
         {/* <img className="headerImage" src="/images/IMG_0055.jpg"></img> */}
         <img className="headerImage" src="/images/tando-jeongok.jpeg"></img>
       </div>
-      <div></div>
+      <div className="descriptionDetailDiv">
+        <p className="descriptionDetail">
+          Explore Hwaseong City. Experience life on the western port of South
+          Korea. Enjoy the flavors of the sea. Learn about the history of
+          Hwaseong. Watch the sunset. Explore a new town. Catch crabs on the
+          mudflats. Ride a cable car over the sea. There's many things to see
+          and do in Hwaseong.
+        </p>
+      </div>
     </div>
   );
 }
