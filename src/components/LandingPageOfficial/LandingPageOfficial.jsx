6929775a7d170623263df009ee85fde66cc31acc
jessiyang1996 <jessiyang1996@gmail.com>
2024-03-14 00:32:25 -0500

    created a new home page

---
+++ src/components/LandingPageOfficial/LandingPageOfficial.jsx
@@ -0,0 +1,11 @@
+import './LandingPageOfficial.css';
+
+function LandingPageOfficial() {
+  return (
+    <div>
+      <img className="mainImage" src="/images/Background2.svg"></img>
+    </div>
+  );
+}
+
+export default LandingPageOfficial;
