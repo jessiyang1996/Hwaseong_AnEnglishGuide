7948589af2443e02d95153d7f91f25b6f99d901e
jessiyang1996 <jessiyang1996@gmail.com>
2024-03-23 09:57:38 -0500

    fixed the navigator drop down menu when logged in

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,63 +1,81 @@
 import './Nav.css';
-import React from 'react';
+import React, { useState } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Nav() {
   const user = useSelector((store) => store.user);
 
+  //the drop down menu should not appear until clicked on
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <div className="navContainer">
-      <img src="/images/logo.png" className="navigationLogo"></img>
+      <img src="/images/logo.png" className="navigationLogo" alt="Logo"></img>
       <div className="navLinkContainer">
-        <div className="linksDiv">
-          {/* If no user is logged in, show these links: */}
-          {!user.id && (
-            <>
-              <Link to="/home" className="navLink">
-                HOME
-              </Link>
+        {/* If no user is logged in, show these links: */}
+        {!user.id && (
+          <>
+            <Link to="/home" className="navLink">
+              HOME
+            </Link>
 
-              <Link to="/about" className="navLink">
-                ABOUT
-              </Link>
+            <Link to="/about" className="navLink">
+              ABOUT
+            </Link>
 
-              <Link to="/explore-hwaseong" className="navLink">
-                EXPLORE HWASEONG
-              </Link>
+            <Link to="/explore-hwaseong" className="navLink">
+              EXPLORE HWASEONG
+            </Link>
 
-              <Link to="/nearby-recommendations" className="navLink">
-                NEARBY RECOMMENDATIONS
-              </Link>
-            </>
-          )}
-          {/* If a user is logged in, show these links: */}
-          {user.id && (
-            <>
-              <Link to="/home" className="navLink">
-                HOME
-              </Link>
+            <Link to="/nearby-recommendations" className="navLink">
+              NEARBY RECOMMENDATIONS
+            </Link>
+          </>
+        )}
+        {/* If a user is logged in, show these links: */}
+        {user.id && (
+          <>
+            <Link to="/home" className="navLink">
+              HOME
+            </Link>
 
-              <Link to="/about" className="navLink">
-                ABOUT
-              </Link>
+            <Link to="/about" className="navLink">
+              ABOUT
+            </Link>
 
-              <Link to="/explore-hwaseong" className="navLink">
-                EXPLORE HWASEONG
-              </Link>
+            <Link to="/explore-hwaseong" className="navLink">
+              EXPLORE HWASEONG
+            </Link>
 
-              <Link to="/nearby-recommendations" className="navLink">
-                NEARBY RECOMMENDATIONS
-              </Link>
+            <Link to="/nearby-recommendations" className="navLink">
+              NEARBY RECOMMENDATIONS
+            </Link>
 
-              <LogOutButton className="navLink" />
-            </>
-          )}
-        </div>
-        {/* End of linksDiv */}
+            <AccountCircleIcon className="navLink" onClick={toggleDropdown} />
+          </>
+        )}
       </div>
       {/* End of navLinkContainer */}
+
+      {/* DROPDOWN MENU */}
+      <div className="dropdownDiv">
+        {showDropdown && (
+          <div className="dropDownItemsDiv">
+            <Link to="/admin-dashboard" className="dropdownItem">
+              Dashboard
+            </Link>
+            <LogOutButton id="logOutButton" className="dropdownItem" />
+          </div>
+        )}
+      </div>
+      {/* End of DROPDOWN MENU */}
     </div>
   );
 }
