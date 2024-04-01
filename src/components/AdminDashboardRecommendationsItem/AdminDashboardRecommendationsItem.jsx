8951c7021fa80f0f63f6b773713520b607d45ab7
jessiyang1996 <jessiyang1996@gmail.com>
2024-03-31 22:12:55 -0500

    the refrest list after deleting post is not working

--- src/components/AdminDashboardRecommendationsItem/AdminDashboardRecommendationsItem.jsx
+++ src/components/AdminDashboardRecommendationsItem/AdminDashboardRecommendationsItem.jsx
@@ -3,9 +3,11 @@ import './AdminDashboardRecommendationsItem.css';
 
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 function RecommendationsItem({ recommendation }) {
   const history = useHistory();
+  const dispatch = useDispatch();
 
   function handleEditButton() {
     // console.log('edit post button was pressed');
@@ -15,22 +17,33 @@ function RecommendationsItem({ recommendation }) {
     // ); // confirmed that the id was being passed correctly
     history.push(`/edit-post/${recommendation.id}`);
     //TODO send the item we clicked to the store
-  }
+  } // end of handleEditButton()
 
   //TODO: CREATE HANDLE DELETE
   function handleDeleteButton() {
     console.log('in handleDeleteButton()');
     axios
       .delete(`/api/recommendations/${recommendation.id}`)
-      .then(() => {
-        // get data from server (refresh the list)
+      .then((response) => {
+        // TODO: get data from server (refresh the list)
+        axios
+          .get('/api/recommendations')
+          .then((response) => {
+            dispatch({
+              type: 'SET_RECOMMENDATIONS_LIST',
+              payload: 'response.data',
+            });
+          })
+          .catch((error) => {
+            console.log('error in refetching data after deleting post', error);
+          });
       })
       .catch((error) => {
         console.log('Error in DELETE route: ', error);
         alert('Something went wrong! Look at the console');
       });
-    // refresh the data (GET the updated list from the server)
   }
+
   return (
     <div className="recommendationsItemDiv">
       <ul>
