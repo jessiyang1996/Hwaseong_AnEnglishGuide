bbfeb347131a3136cf31e5e0e14bbce738ba3e2d
jessiyang1996 <jessiyang1996@gmail.com>
2024-04-08 23:14:34 -0500

    reworking the image map

---
+++ src/components/ExploreHwaseongDetails/ExploreHwaseongDetails.js
@@ -0,0 +1,16 @@
+// data for Hwaseong image map
+const hwaseongImageMapData = [
+  {
+    name: 'Seoshin-myeon',
+    details:
+      "Seoshin-myeon is a small fishing village on the coast of Hwaseong. Although it is a small rural town, there are many things to see and do here. Famous for it's sunsets, fresh seafood, Jebu Island, and ports, many people spend the evening here with their friends and family. ",
+    image1: '/images/',
+  },
+  {
+    name: 'Songsan-Myeon',
+    details:
+      'Songsan-myeon is a growing small town. The main street of Songsan sees a lot of traffic as a lot of people drive pass here to get to the ports or factories. There is a popular fish market called Sagang Market (사강시장).',
+  },
+];
+
+export default hwaseongImageMapData;
